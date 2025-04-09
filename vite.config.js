9f7264e4cd04ecc15b4ac1bf7d72ce02681d2869
Citizshrestha <citizshresthaa@gmail.com>
2025-04-09 09:49:14 +0545

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+// vite.config.js (ESM)
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@backend': path.resolve(import.meta.dirname, 'src/backend'),
+      '@frontend': path.resolve(import.meta.dirname, 'src/frontend'),
+      '@assets': path.resolve(import.meta.dirname, 'src/assets'),
+      '@utils': path.resolve(import.meta.dirname, 'src/utils'),
+    },
+  },
 });

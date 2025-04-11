4e33c2fac2b512236bd1e3aa6233c08fdb8a00c4
Citizshrestha <citizshresthaa@gmail.com>
2025-04-11 18:30:56 +0545

    emp  add/ edit func

--- src/frontend/components/Dashboard/EmployeeDashboard.jsx
+++ src/frontend/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
 
-const EmployeeDashboard = () => {
+import Sidebar from '@frontend/components/common/Sidebar';
+import Header from '@frontend/components/common/Header';
+
+
+
+const AdminDashboard = () => {
+  
+
+
   return (
-    <div>
-       Emp page
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="p-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+             
+          </div>
+        </main>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeDashboard
+export default AdminDashboard;

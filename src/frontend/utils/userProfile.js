97770a11d3f11e74d354d4819535f47138b3bfa1
Citizshrestha <citizshresthaa@gmail.com>
2025-04-11 18:30:07 +0545

    helper func

---
+++ src/frontend/utils/userProfile.js
@@ -0,0 +1,27 @@
+
+import { auth, db } from "../../backend/services/supabaseClient";
+
+
+   export const fetchUserProfile =  async () =>{
+    try {
+        const {data:{user} , error: userError} =  await auth.getUser()
+              if (userError || !user){
+                throw new Error('No authenticated user found')
+              }
+
+              const {data, err} = await db
+              .from('profiles')
+                .select('name,role,avatarurl')
+                .eq('id',user.id)
+                .single()
+
+                if (err){
+                    throw err;
+                } 
+                return data;
+    } catch (error) {
+        console.error("Error fetching user data",error)
+    }
+   }
+
+
